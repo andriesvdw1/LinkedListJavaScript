2c3c961688f835a4e5e89df5a40a0196227c60fe
andriesvdw1 <78602632+andriesvdw1@users.noreply.github.com>
2025-11-30 21:02:23 +0200

    Add method to LinkedList for adding nodes
    Implemented add method to LinkedList class.

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ class LinkedList {
         this.head = null;
         this.length = 0;
     }
+    add(data){
+        const newNode = new Node(data);
+        if(this.head === null) {
+            this.head = newNode;
+        }
+        
+    }
 }
+
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+console.log(list);
